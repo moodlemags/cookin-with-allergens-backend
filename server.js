ec24c1f228d899320477d48712007b44c4dbf201
Maggie Merrell <maggiemerrell31@gmail.com>
2016-07-21 16:05:17 -0400

    all working back end

---
+++ server.js
@@ -0,0 +1,40 @@
+var express     = require('express');
+var cors        = require('cors');
+var bodyParser  = require('body-parser');
+var mongodb     = require('mongodb');
+var request     = require('request');
+var app         = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(request, response){
+  response.json({"description": "My back end is up and running"})
+})
+
+
+app.post('/get', function(req, res){
+
+  var baseURL = 'http://api.yummly.com/v1/api/';
+  var YUMMLY_USER = process.env.YUMMLY_USER;
+  var YUMMLY_KEY = process.env.YUMMLY_KEY;
+  console.log(YUMMLY_USER, YUMMLY_KEY);
+  var urlDirect = 'q=' + req.body.querySelector;
+  var allergenInfo = req.body.allergens;
+  var fullURL = baseURL + 'recipes?' + '_app_id=' + YUMMLY_USER + '&_app_key=' + YUMMLY_KEY + '&' + urlDirect + allergenInfo;
+  console.log('full URL : ', fullURL);
+
+  request ({
+    url: fullURL,
+    method: 'GET',
+    callback: function(error, response, body) {
+      res.send(body)
+    }
+  });
+
+})// end post fxn
+
+
+app.listen(3000, function(){
+  console.log('listening to events');
+})
